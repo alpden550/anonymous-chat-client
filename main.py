e54f8d2d417a5863da9204f8f7d965f3c1fd5da5
Denis Novikov <alpden550@gmail.com>
2020-10-30 18:21:45 +0300

    feat(main) add infinity messaging into chat

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import asyncio
+import time
 
 import gui
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
 
+async def generate_msgs(queue: asyncio.Queue) -> None:
+    while True:
+        queue.put_nowait(f'Ping {round(time.time())}')
+        await asyncio.sleep(1)
+
+
+async def main() -> None:
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
-    loop.run_until_complete(
-        gui.draw(messages_queue, sending_queue, status_updates_queue)
-    )
+    asyncio.gather(generate_msgs(messages_queue))
+
+    await gui.draw(messages_queue, sending_queue, status_updates_queue)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
