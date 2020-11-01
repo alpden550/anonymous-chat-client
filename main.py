5dde740e16e3deb22fc393d11d5ba7bd76a126f0
alpden550 <alpden550@gmail.com>
2020-11-01 21:06:12 +0300

    feat(main) add user authorization wuth token

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from asyncio.streams import StreamReader, StreamWriter
 from datetime import datetime
 
 import aiofiles
@@ -7,20 +9,18 @@ import click
 import gui
 
 
-async def read_msgs(
-    host: str, port: int, queue: asyncio.Queue, history_queue: asyncio.Queue,
-) -> None:
-    try:
-        reader, writer = await asyncio.open_connection(host=host, port=port)
+ASYNC_DELAY = 2
 
-        while True:
-            line = await reader.readline()
-            if not line:
-                break
-            queue.put_nowait(line.decode())
-            history_queue.put_nowait(line.decode())
-    finally:
-        writer.close()
+
+async def read_msgs(
+    reader: StreamReader, queue: asyncio.Queue, history_queue: asyncio.Queue,
+) -> None:
+    while True:
+        line = await reader.readline()
+        if not line:
+            break
+        queue.put_nowait(line.decode())
+        history_queue.put_nowait(line.decode())
 
 
 async def write_history(history: asyncio.Queue, logfile: str):
@@ -39,30 +39,60 @@ async def load_history(logfile: str, queue: asyncio.Queue):
             queue.put_nowait(template.format(history))
 
 
-async def send_msgs(host: str, port: int, queue: asyncio.Queue):
+async def send_msgs(host: str, port: int, queue: asyncio.Queue) -> None:
     while True:
         message = await queue.get()
 
 
-async def start_chat(host: str, port: int, output: str) -> None:
+async def authorize_chat_user(token: str, writer: StreamWriter) -> None:
+    writer.write(f'{token}\n'.encode())
+    await writer.drain()
+
+
+async def handle_user(
+    host: str,
+    port: int = 5050,
+    token: str = None,
+    messages_queue: asyncio.Queue = None,
+) -> None:
+    try:
+        reader, writer = await asyncio.open_connection(host=host, port=port)
+        await reader.readline()
+
+        if token:
+            await authorize_chat_user(token=token, writer=writer)
+            authorize_data = await reader.readline()
+            user = json.loads(authorize_data)['nickname']
+            message = f'Выполнена авторизация. Пользователь {user}.\n\n'
+            messages_queue.put_nowait(message)
+            await asyncio.sleep(ASYNC_DELAY)
+    finally:
+        writer.close()
+
+
+async def start_chat(host: str, port: int, output: str, token: str) -> None:
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
     history_queue = asyncio.Queue()
 
-    asyncio.gather(
-        load_history(logfile=output, queue=messages_queue),
-        read_msgs(
-            host=host,
-            port=port,
-            queue=messages_queue,
-            history_queue=history_queue,
-        ),
-        write_history(history=history_queue, logfile=output),
-        send_msgs(host=host, port=port, queue=sending_queue),
-    )
+    try:
+        reader, writer = await asyncio.open_connection(host=host, port=port)
 
-    await gui.draw(messages_queue, sending_queue, status_updates_queue)
+        asyncio.gather(
+            handle_user(host=host, token=token, messages_queue=messages_queue),
+            load_history(logfile=output, queue=messages_queue),
+            read_msgs(
+                reader=reader,
+                queue=messages_queue,
+                history_queue=history_queue,
+            ),
+            write_history(history=history_queue, logfile=output),
+            send_msgs(host=host, port=port, queue=sending_queue),
+        )
+        await gui.draw(messages_queue, sending_queue, status_updates_queue)
+    finally:
+        writer.close()
 
 
 @click.command()
@@ -96,7 +126,7 @@ async def start_chat(host: str, port: int, output: str) -> None:
     show_default=True,
 )
 def main(host: str, port: int, token: str, output: str):
-    asyncio.run(start_chat(host=host, port=port, output=output))
+    asyncio.run(start_chat(host=host, port=port, output=output, token=token))
 
 
 if __name__ == "__main__":
