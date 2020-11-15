3d4a0fb81b3b257277ef6082fcaa855b0b94a781
alpden550 <alpden550@gmail.com>
2020-11-15 19:10:11 +0300

    style(interfaces) update with flake

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,4 +1,4 @@
-from interfaces import gui
-from interfaces.reader_interface import ChatReaderInterface
-from interfaces.wathcer_interface import ChatWatcherInterface
-from interfaces.writer_interface import ChatWriterInterface
+from interfaces import gui  # noqa
+from interfaces.reader_interface import ChatReaderInterface  # noqa
+from interfaces.wathcer_interface import ChatWatcherInterface  # noqa
+from interfaces.writer_interface import ChatWriterInterface  # noqa
