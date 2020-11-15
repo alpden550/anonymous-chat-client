5ddab239fb0a696c706c33f05f610713dd5d15ae
alpden550 <alpden550@gmail.com>
2020-11-15 17:18:01 +0300

    feat(interfaces) add watcher interface inti inti

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,3 +1,4 @@
 from interfaces import gui
 from interfaces.reader_interface import ChatReaderInterface
+from interfaces.wathcer_interface import ChatWatcherInterface
 from interfaces.writer_interface import ChatWriterInterface
