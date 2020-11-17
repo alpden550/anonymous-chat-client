904c23325e147fb200e78b823f4f8bbd541cebca
alpden550 <alpden550@gmail.com>
2020-11-17 17:20:19 +0300

    style(watcher) fix typo

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,4 +1,4 @@
 from interfaces import gui  # noqa
 from interfaces.reader_interface import ChatReaderInterface  # noqa
-from interfaces.wathcer_interface import ChatWatcherInterface  # noqa
+from interfaces.watcher_interface import ChatWatcherInterface  # noqa
 from interfaces.writer_interface import ChatWriterInterface  # noqa
